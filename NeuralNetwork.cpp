a232c9f3c2fee05c8e1a6a33638a2e2fe3ca7be6
Crescenzo Garofalo <crescenzogarofalo@MBPdiCrescenzo.homenet.telecomitalia.it>
2020-03-29 19:02:23 +0200

    back propagate

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -2,51 +2,34 @@
 #include <vector>
 #include "NeuralNetwork.h"
 
-
-linalg::Matrix::Matrix(int nr, int nc) :numRows(nr), numCols(nc) {
-	this->elements = new double* [numRows];
+template <typename T>
+linalg::Matrix<T>::Matrix(int nr, int nc) :numRows(nr), numCols(nc) {
+	this->elements = new T* [numRows];
 	for (int i = 0; i < numRows; i++) {
-		this->elements[i] = new double[numCols];
+		this->elements[i] = new T[numCols];
 		for (int j = 0; j < numCols; j++) {
 			this->elements[i][j] = 0.0;
 		}
 	}
 }
 
-double** linalg::Matrix::getElements() {
+template <typename T>
+T** linalg::Matrix<T>::getElements() {
 	return this->elements;
 }
 
-int linalg::Matrix::getNumRows() {
+template <typename T>
+int linalg::Matrix<T>::getNumRows() {
 	return numRows;
 }
 
-int linalg::Matrix::getNumCols() {
+template <typename T>
+int linalg::Matrix<T>::getNumCols() {
 	return numCols;
 }
 
-linalg::Matrix* linalg::operator*(linalg::Matrix& a, linalg::Matrix& b) {
-	int nr = a.getNumRows();
-	int nc = b.getNumCols();
-	int nca = a.getNumCols();
-	int nrb = b.getNumRows();
-	linalg::Matrix* res = new linalg::Matrix(nr, nc);
-	if (nca == nrb) {
-		for (int i = 0; i < nr; i++) {
-			for (int j = 0; j < nc; j++) {
-				double sp = 0.0;
-				for (int jj = 0; jj < nrb; jj++) {
-					sp += a.getElements()[i][jj] * b.getElements()[jj][j];
-				}
-				res->elements[i][j] = sp;
-			}
-		}		
-	}
-	
-	return res;
-}
-
-void linalg::Matrix::print(void) {
+template <typename T>
+void linalg::Matrix<T>::print(void) {
 	std::cout << "\nMatrix: \n";
 	for (int i = 0; i < numRows; i++) {
 		for (int j = 0; j < numCols; j++) {
@@ -57,7 +40,7 @@ void linalg::Matrix::print(void) {
 }
 
 nn::FullyConnected::FullyConnected(int ni, int no):numInputs(ni), numOutputs(no) {
-	this->weights = new linalg::Matrix(numOutputs, numInputs);	
+	this->weights = new linalg::Matrix<double>(numOutputs, numInputs);
 	for (int i = 0; i < numOutputs; i++) {
 		for (int j = 0; j < numInputs; j++) {
 			this->weights->getElements()[i][j] = ((double)rand() / (RAND_MAX));
@@ -65,28 +48,28 @@ nn::FullyConnected::FullyConnected(int ni, int no):numInputs(ni), numOutputs(no)
 	}
 }
 
-linalg::Matrix* nn::FullyConnected::getWeights() {
+linalg::Matrix<double>* nn::FullyConnected::getWeights() {
 	return this->weights;
 }
 
-linalg::Matrix* nn::FullyConnected::getInput() {
+linalg::Matrix<double>* nn::FullyConnected::getInput() {
 	return this->input;
 }
 
-linalg::Matrix* nn::FullyConnected::getOutput() {
+linalg::Matrix<double>* nn::FullyConnected::getOutput() {
 	return this->output;
 }
 
-linalg::Matrix* nn::FullyConnected::feedForward(linalg::Matrix* input) {
+linalg::Matrix<double>* nn::FullyConnected::feedForward(linalg::Matrix<double>* input) {
 	this->input = input;
 	this->output = (*this->weights) * (*input);
-	return this->output;
+	return this->getOutput();
 }
 
-void nn::FullyConnected::backPropagate(linalg::Matrix& expected, double learningRate) {
+void nn::FullyConnected::backPropagate(linalg::Matrix<double>& expected, double learningRate) {
 	int nr = this->numOutputs;
 	int nc = this->numInputs;
-	linalg::Matrix* weightsMatrix = this->getWeights();
+	linalg::Matrix<double>* weightsMatrix = this->getWeights();
 	double diffOut = 0.0;
 	for (int i = 0; i < nr; i++) {
 		diffOut += expected.getElements()[0][i] - this->output->getElements()[0][i];
@@ -106,7 +89,7 @@ nn::NeuralNet::NeuralNet(int nl):numOfLayers(nl) {
     this->layers = new std::vector<FullyConnected>(nl);
 }
 
-void nn::NeuralNet::learn(linalg::Matrix& trainingSet) {
+void nn::NeuralNet::learn(linalg::Matrix<double>& trainingSet) {
     /*
      training set
      [[x1,y1],[x2,y2],...,[xn,yn]]
--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -3,24 +3,51 @@
 #include <vector>
 
 namespace linalg {
+    template <typename T>
+    class Matrix;
+    
+    template <typename T>
+    Matrix<T>* operator*(Matrix<T>& a, Matrix<T>& b);
 
+    template <typename T>
 	class Matrix {
 	private:
 		int numRows;
 		int numCols;
-		double** elements;
+		T** elements;
 
 	public:
 		Matrix(int nr, int nc);
 
-		friend Matrix* operator*(Matrix& a, Matrix& b);
-
-		double** getElements();
+		T** getElements();
 		int getNumRows(void);
 		int getNumCols(void);
 
 		void print(void);
+        
+        friend Matrix<T>* operator*(Matrix<T>& a, Matrix<T>& b) {
+            int nr = a.getNumRows();
+            int nc = b.getNumCols();
+            int nca = a.getNumCols();
+            int nrb = b.getNumRows();
+            linalg::Matrix<T>* res = new linalg::Matrix<T>(nr, nc);
+            if (nca == nrb) {
+                for (int i = 0; i < nr; i++) {
+                    for (int j = 0; j < nc; j++) {
+                        double sp = 0.0;
+                        for (int jj = 0; jj < nrb; jj++) {
+                            sp += a.getElements()[i][jj] * b.getElements()[jj][j];
+                        }
+                        res->elements[i][j] = sp;
+                    }
+                }
+            }
+            
+            return res;
+        }
 	};
+
+    
 }
 
 namespace nn {
@@ -28,10 +55,10 @@ namespace nn {
 	class FullyConnected {
 
 	private:
-		linalg::Matrix* weights;
+		linalg::Matrix<double>* weights;
 		int numInputs, numOutputs;
-		linalg::Matrix* input;
-		linalg::Matrix* output;
+		linalg::Matrix<double>* input;
+		linalg::Matrix<double>* output;
 
 		FullyConnected* previousLayer;
 
@@ -39,12 +66,12 @@ namespace nn {
 
 		FullyConnected(int ni, int no);
 
-		linalg::Matrix* getWeights();
-		linalg::Matrix* getInput();
-		linalg::Matrix* getOutput();
+		linalg::Matrix<double>* getWeights();
+		linalg::Matrix<double>* getInput();
+		linalg::Matrix<double>* getOutput();
 
-		linalg::Matrix* feedForward(linalg::Matrix* input);
-		void backPropagate(linalg::Matrix& expected, double learningRate);
+		linalg::Matrix<double>* feedForward(linalg::Matrix<double>* input);
+		void backPropagate(linalg::Matrix<double>& expected, double learningRate);
 
 	};
 
@@ -57,6 +84,6 @@ namespace nn {
     public:
         NeuralNet(int nl);
         
-        void learn(linalg::Matrix& trainingSet);
+        void learn(linalg::Matrix<double>& trainingSet);
 	};
 }
